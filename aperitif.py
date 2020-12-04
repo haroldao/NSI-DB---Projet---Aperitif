0f8b7302949dd295c1780b8897d6eb12dff535e1
haroldao <harold86340@gmail.com>
2020-12-04 20:02:56 +0100

    Init Application function

--- aperitif.py
+++ aperitif.py
@@ -14,3 +14,24 @@ Quelle action souhaitez vous effectuez ? répondre par 1,2,3,4 ou 5:
 4 - D - Supprimer un article ou une personne.
 5 - Quitter l'application.
 """
+
+
+######################## Application #######################
+
+
+def app():
+
+    while(user_input := input(MENU_PROMPT)) != "5":
+        if user_input == "1":
+            pass
+        elif user_input == "2":
+            pass
+        elif user_input == "3":
+            pass
+        elif user_input == "4":
+            pass
+        else:
+            print("Oups... 😥... essayez encore !")
+
+
+app()
